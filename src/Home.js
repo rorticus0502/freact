2a842c1ccc1ce993d74c36c4cb25c5132218ea0f
rorticus0502 <rorticus0502@gmail.com>
2023-03-09 22:00:56 +0000

    drawing correctly

--- src/Home.js
+++ src/Home.js
@@ -26,34 +26,17 @@ const Home = () => {
     const [display, setDisplay] = useState(logo);
     const [displayClass, setDisplayClass] = useState('App-logo');
     const [zooms, setZooms] = useState([]);
+    const [topLeftX, setTopLeftX] = useState([0]);
+    const [topLeftY, setTopLeftY] = useState([0]);
+    const [bottomRightX, setBottomRightX] = useState([1200]);
+    const [bottomRightY, setBottomRightY] = useState([900]);
+    const [minr, setMinr] = useState([-2.0]);
+    const [maxr, setMaxr] = useState([1.0]);
+    const [mini, setMini] = useState([-1.5]);
+    const [maxi, setMaxi] = useState([1.5]);
 
     const login = async () => oktaAuth.signInWithRedirect();
     const logout = async () => oktaAuth.signOut('/');
-
-    var  findOut = () => {
-
-            const config = {
-                headers: {
-                    Authorization: 'Bearer ' + accessToken
-                }
-            }
-
-            axios.get('http://localhost:8080/api/init', config)
-                    .then(function(response) {
-                        console.log(response);
-                        setDisplay(`data:image/jpg;base64,${response.data.encodedImage}`);
-                        setDisplayClass('fractal-display');
-
-                        const newZooms= zooms.slice();
-                        newZooms.push(buildAZoom(response.data.zoom));
-                        setZooms(newZooms);
-
-                    })
-                    .catch(function(error) {
-                        console.log(error);
-                    });
-
-        };
 
     useEffect(() => {
 
@@ -72,13 +55,73 @@ const Home = () => {
         );
     }
 
+    const handleMouseDown = (event) => {
+        event.preventDefault();
+
+        topLeftX[0] = event.nativeEvent.offsetX;
+        topLeftY[0] = event.nativeEvent.offsetY;
+
+        console.log(`mouse down ${event.nativeEvent.offsetX} ${event.nativeEvent.offsetY}`);
+
+    }
+
+    const handleMouseRelease = (event) => {
+        event.preventDefault();
+
+        bottomRightX[0] = event.nativeEvent.offsetX;
+        bottomRightY[0] = event.nativeEvent.offsetY;
+
+        console.log(`mouse release ${event.nativeEvent.offsetX} ${event.nativeEvent.offsetY}`);
+
+        zoom();
+    }
+
+    const zoom = () => {
+
+        const config = {
+            headers: {
+                Authorization: 'Bearer ' + accessToken
+            }
+        }
+
+        var params = new URLSearchParams();
+        params.append('toplx', topLeftX[0]);
+        params.append('toply', topLeftY[0]);
+        params.append('botrx', bottomRightX[0]);
+        params.append('botry', bottomRightY[0]);
+
+        params.append('minr', minr[0]);
+        params.append('maxr', maxr[0]);
+        params.append('mini', mini[0]);
+        params.append('maxi', maxi[0]);
+
+        var zoomUrl = 'http://localhost:8080/api/zoom?' + params.toString();
+
+        axios.get(zoomUrl, config)
+        .then(function(response) {
+            setDisplay(`data:image/jpg;base64,${response.data.encodedImage}`);
+            setDisplayClass('fractal-display');
+
+            const newZooms= zooms.slice();
+            newZooms.push(buildAZoom(response.data.zoom));
+            setZooms(newZooms);
+            setMinr([response.data.zoom.realMin]);
+            setMaxr([response.data.zoom.realMax]);
+            setMini([response.data.zoom.imaginaryMin]);
+            setMaxi([response.data.zoom.imaginaryMax]);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    }
+
     return (
         <div class="App-header">
-            <button onClick={findOut}>Whatsitnow</button>
+            <button onClick={zoom}>Whatsitnow</button>
             <button onClick={logout}>Logout</button>
             <div id="mandelbrot-wrapper">
                 <div id="fractal-wrapper">
-                    <img src={display} className={displayClass} alt="logo" />
+                    <img src={display} className={displayClass} alt="logo" onMouseDown={handleMouseDown} onMouseUp={handleMouseRelease} />
                 </div>
                 <div id="zooms-panel-wrapper">
                     <div id="zooms-panel">

ae522eb794e1a13f9e9b5493187205422954358f
rorticus0502 <rorticus0502@gmail.com>
2023-03-13 20:30:39 +0000

    added info panels to home

--- src/IntroPanel.js
+++ src/IntroPanel.js
@@ -4,8 +4,20 @@ const IntroPanel = () => {
 
     return (
         <div id="intro-wrapper">
-            <div id="fractal-wrapper">
+            <div id="intro-info-left" className="intro-info">
+                <div>
+                    This image was generated in the region;
+                    <p>Real axis -0.199 to -0.191</p>
+                    Imaginary axis 0.665 to 0.675
+                </div>
+            </div>
+            <div>
                 <img src={initImage} className='init-style' />
+            </div>
+            <div id="intro-info-right" className="intro-info">
+                <div>
+                    Zn = Z^2 + C
+                </div>
             </div>
         </div>
     );
